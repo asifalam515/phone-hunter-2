c44329c0d64110861412f9e5bc124102e042c25a
asifalam515 <asibulalam515@gmail.com>
2022-10-16 18:25:00 +0600

    search function added

---
+++ js/app.js
@@ -0,0 +1,38 @@
+const loadPhones=(searchText)=>{
+    const url=`https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>displayPhones(data.data))
+}
+
+const displayPhones=(phones)=>{
+    
+   const phonesContainer=document.getElementById('phone-container')
+    
+   phones.forEach(phone=>{
+    console.log(phone);
+   const phoneDiv=document.createElement('div')
+   phoneDiv.classList.add('col')
+   phoneDiv.innerHTML=`
+  
+   <div class="card m-2 p-2" style="width: 18rem; ">
+  <img src="${phone.image}" class="card-img-top" alt="...">
+  <div class="card-body p-4 m-2">
+    <h5 class="card-title">${phone.phone_name}</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>`
+phonesContainer.appendChild(phoneDiv)
+
+
+   })
+}
+
+document.getElementById('btn-search').addEventListener('click',function(){
+    const searchField=document.getElementById('search-field')
+    const searchText=searchField.value;
+loadPhones(searchText)
+})
+
+loadPhones()
